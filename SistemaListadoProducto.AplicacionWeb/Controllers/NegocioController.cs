9816e0fa4a529a1502d155e1fb4a9b58c36a8ffb
Nicolas Gravertt <nicolasgravertt@hotmail.com>
2024-01-30 19:39:35 -0300

    Cambios

---
+++ SistemaListadoProducto.AplicacionWeb/Controllers/NegocioController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SistemaListadoProducto.AplicacionWeb.Controllers
+{
+    public class NegocioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SistemaListadoProducto.AplicacionWeb/Controllers/NuevaVentaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SistemaListadoProducto.AplicacionWeb.Controllers
+{
+    public class NuevaVentaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
